a17d3d5b240176befb04f286a0740760f74290b0
Austin Rowe <amrowe2018@gmail.com>
2019-02-14 22:53:44 -0600

    Cleaned up nav for better functionality.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,50 +6,19 @@ class Nav extends Component {
   constructor(){
     super();
     this.state = {
-      menuDisplayNone: true,
-      projectsVisible: false,
-      aboutVisible: false,
-      resumeVisible: false,
-      contactVisible: false,
+      menuVisible: false,
       menuToggled: false
     }
     this.menuToggle = this.menuToggle.bind(this);
     this.hideMenu = this.hideMenu.bind(this);
-    this.toggleMenuOption = this.toggleMenuOption.bind(this);
-  }
-
-  toggleMenuOption(option, time){
-    setTimeout(() => {this.setState(state => ({
-      [option]: !state[option]
-    }))}, time);
   }
 
   menuToggle(){
-    let time = 0;
-    Object.keys(this.state).forEach(option => {
-      if(option === "menuToggled"){
-        setTimeout(() => {this.setState({menuToggled: true})}, time);
-      } else if(option === "menuDisplayNone"){
-        this.state.menuDisplayNone? this.toggleMenuOption(option, time) : this.toggleMenuOption(option, 750);
-      } else {
-        this.toggleMenuOption(option, time);
-      }
-      time = time + 50;
-    });
+    this.setState(state => ({menuVisible: !state.menuVisible, menuToggled: true}));
   }
 
   hideMenu(){
-    let time = 0;
-    Object.keys(this.state).forEach(option => {
-      if(option === "menuToggled"){
-        return;
-      } else if(option === "menuDisplayNone"){
-        setTimeout(() => {this.setState({[option]: true})}, 750);
-      } else {
-        setTimeout(() => {this.setState({[option]: false})}, time);
-      }
-      time = time + 50;
-    })
+    this.setState({menuVisible: false});
   }
 
   render() {
@@ -60,25 +29,11 @@ class Nav extends Component {
           <div className={this.props.color? "color drop-down-toggle-bar": "drop-down-toggle-bar"}></div>
           <div className={this.props.color? "color drop-down-toggle-bar": "drop-down-toggle-bar"}></div>
         </div>
-        <div id="menu" className={this.state.menuDisplayNone? "none" : ""} onMouseLeave={this.hideMenu}>
-          <a onClick={this.hideMenu} className={this.state.projectsVisible? "visible" : this.state.menuToggled? "menu-link hidden" : "menu-link"} href="#projects-container"><h2 className={this.props.color? "color menu-option": "menu-option"}>PROJECTS</h2></a>
-          <a onClick={this.hideMenu} className={this.state.aboutVisible? "visible" : this.state.menuToggled? "menu-link hidden" : "menu-link"} href="#about-container" ><h2 className={this.props.color? "color menu-option": "menu-option"}>ABOUT</h2></a>
-          <a onClick={this.hideMenu} className={this.state.resumeVisible? "visible" : this.state.menuToggled? "menu-link hidden" : "menu-link"} href="/Austin-Rowe.pdf" target="_blank" rel="noopener noreferrer"><h2 className={this.props.color? "color menu-option": "menu-option"}>R&Eacute;SUM&Eacute;</h2></a>
-          <div id="contact-link" onClick={this.hideMenu} className={this.state.contactVisible? "visible" : this.state.menuToggled? "menu-link hidden" : "menu-link"} >
-            <a href="#contact-container" ><h2 className={this.props.color? "color menu-option": "menu-option"}>CONTACT</h2></a>
-            <a className={this.props.color? "sub-color sub-menu-option": "sub-menu-option"} href="tel:+1-205-807-4715">
-                <img className="contact-icon" src="/phoneIcon.svg" alt=""/>
-                <h3 className="sub-option-text">205-807-4715</h3>
-            </a>
-            <a className={this.props.color? "sub-color sub-menu-option": "sub-menu-option"} href="mailto:amrowe2018@gmail.com">
-                <img className="contact-icon" src="/emailIcon.svg" alt=""/>
-                <h3 className="sub-option-text">amrowe2018@gmail.com</h3>
-            </a>
-            <a className={this.props.color? "sub-color sub-menu-option": "sub-menu-option"} href="http://github.com/Austin-Rowe" target="_blank" rel="noopener noreferrer">
-                <img className="contact-icon" src="/githubIcon.svg" alt=""/>
-                <h3 className="sub-option-text">GitHub</h3>
-            </a>
-          </div>
+        <div className={this.state.menuVisible? "visible menu" : this.state.menuToggled? "hidden menu" : "menu"} onMouseLeave={this.hideMenu}>
+          <a onClick={this.menuToggle} href="#projects-container"><h2 className={this.props.color? "color menu-option": "menu-option"}>PROJECTS</h2></a>
+          <a onClick={this.menuToggle} href="#about-container" ><h2 className={this.props.color? "color menu-option": "menu-option"}>ABOUT</h2></a>
+          <a onClick={this.menuToggle} href="/Austin-Rowe.pdf" target="_blank" rel="noopener noreferrer"><h2 className={this.props.color? "color menu-option": "menu-option"}>R&Eacute;SUM&Eacute;</h2></a>
+          <a onClick={this.menuToggle} href="#contact-container" ><h2 className={this.props.color? "color menu-option": "menu-option"}>CONTACT</h2></a>
         </div>
       </React.Fragment>
     );
